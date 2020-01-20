a4b1b5497efad4e238e25bcebc4a4ac7a9138bab
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-01-20 09:12:06 +0100

    Completed the face detection box

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
   }
 
@@ -38,16 +39,29 @@ onInputChange = (event) => {
   this.setState({input: event.target.value});
 }
 
+calculateFaceLocation = (data) => {
+  const clarifaiFaceRegion = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFaceRegion.left_col * width,
+    topRow: clarifaiFaceRegion.top_row * height,
+    rightCol: width - (clarifaiFaceRegion.right_col * width),
+    bottomRow: height - (clarifaiFaceRegion.bottom_row * height)
+  }
+}
+
+displayFaceBox = (box) => {
+  console.log(box);
+  this.setState({box: box});
+}
+
 onButtonSubmit = () => {
   this.setState({imageUrl: this.state.input})
-  app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err) {
-      // there was an error
-    }
-  );
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+  .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+  .catch(err => console.log(err))
 }
 
   render() {
@@ -60,7 +74,7 @@ onButtonSubmit = () => {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   
